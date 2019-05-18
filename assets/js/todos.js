f74bdf8d5f25e871de182e6594896b743b663e0d
MikeWick <wickert.michael@gmail.com>
2019-05-17 21:19:44 -0400

    Create initial file tree: - add basic html structure and link css and js files to index.html - give div container style properties - add initial color and strikethrough functionality on click

---
+++ assets/js/todos.js
@@ -0,0 +1,16 @@
+//Check off Specific Todos by Clicking
+$("li").click(function(){
+  //if li is gray
+  if($(this).css("color") === "rgb(128, 128, 128)"){
+    //turn it black
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  } else {
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }  
+});
